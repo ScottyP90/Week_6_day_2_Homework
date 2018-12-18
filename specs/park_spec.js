b30542fba0f961c76a5cd3f63d2c45a6ef3eccfb
Iain Scott Prentice <iainscottyp@hotmail.com>
2018-12-18 18:49:09 +0000

    park test 2 passed

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -13,7 +13,10 @@ describe('Park', function() {
     assert.strictEqual(actual, 'Jurassic Park')
   });
 
-  it('should have a ticket price');
+  it('should have a ticket price', function() {
+    const actual = park.ticket_price
+    assert.strictEqual(actual, 15)
+  });
 
   it('should have a collection of dinosaurs');
 
